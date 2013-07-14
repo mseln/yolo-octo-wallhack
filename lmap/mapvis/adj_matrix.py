2b7e5d62beb664fe20c82983db8d68a79f6892e4
mseln <mange.selin@gmail.com>
2013-07-15 00:03:02 +0200

    added floyd warshall

--- lmap/mapvis/adj_matrix.py
+++ lmap/mapvis/adj_matrix.py
@@ -4,16 +4,22 @@ INF = 1000000000
 
 class AdjMatrix:
 	def __init__(self, node_refs, nodes, edges):
+		self.node_refs = node_refs
 		self.dist = dict()
-		for i in node_refs.values() :
+		for i in self.node_refs.values() :
 			self.dist[i] = dict()
-			for j in node_refs.values() :
-				self.dist[i][j] = INF
-
+			for j in self.node_refs.values() :
+				if i == j :
+					self.dist[i][j] = 0
+				else :
+					self.dist[i][j] = INF
+		
 		for i in edges.values() :
 			dist = length_haversine(nodes[i.f], nodes[i.t])
 			self.dist[i.f][i.t] = dist
 			self.dist[i.t][i.f] = dist
+
+		self.perform_floyd_warshall()
 
 	def print_adjmat(self) :
 		txt_f = open('adj_mat.txt','w')
@@ -23,4 +29,8 @@ class AdjMatrix:
 			txt_f.write('\n')
 		txt_f.close()
 
-
+	def perform_floyd_warshall(self) :
+		for k in self.node_refs.values() :
+			for i in self.node_refs.values() :
+				for j in self.node_refs.values() :
+					self.dist[i][j] = min(self.dist[i][k] + self.dist[k][j], self.dist[i][j])
