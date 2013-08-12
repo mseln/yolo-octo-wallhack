f0aa71c4807ca5e3f81db71e81700d013e9bf489
mseln <mange.selin@gmail.com>
2013-08-12 11:04:13 +0200

    fixed instructions on website. added unittest for select_nodes_in_rectangle.py

--- lmap2/mapvis/store.py
+++ lmap2/mapvis/store.py
@@ -143,10 +143,11 @@ def extract_osm_nodes(f_name):
 
 def select_nodes_in_rectangle(nodes, min_lat, max_lat, min_lng, max_lng):
     # actual min and max latitude and longitude of coordinates
+
     nodes_in_rectangle = NodeSet()
     for k, node in nodes.return_nodes().items():
-        if(min_lng <= node.lng and node.lng <= max_lng and
-           min_lat <= node.lat and node.lat <= max_lat):
+        if(min_lat <= node.lat and node.lat <= max_lat and 
+           min_lng <= node.lng and node.lng <= max_lng):
             nodes_in_rectangle.add(k, (node.lat, node.lng))
  
     return nodes_in_rectangle
