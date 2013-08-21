88393d71e8883c67ae1b79e01568ec7563fd0e47
Fredrik Heintz <frehe@ida.liu.se>
2013-08-21 16:49:01 +0200

    Updated the project description.

--- code-on-webpage/debug_print.py
+++ code-on-webpage/debug_print.py
@@ -1,7 +1,8 @@
-# Example of usage:
-print(a) # Simply outputs the value of a
-# What if I want to output two variables on the same line?
-print(a + b) # Outputs the sum of a and b if they are compatible
-print(a + ' ' + b) # Won't work if a and b aren't strings
+# Example usage:
+print(a) # Prints the value of the variable a
+
+# What if I want to print two variables on the same line?
+print(a + b) # Prints the sum of a and b, if they are compatible
+print(a + ' ' + b) # Only works if a and b are strings
 # So what's the solution? Easy, simply convert the values to strings.
 print(str(a) + ' ' + str(b))
